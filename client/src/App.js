c5f7ab1d5715485947eaa1bed82ba6b2af4ee3b9
ritesh <riteshsingh9827@gmail.com>
2024-04-21 23:43:34 +0530

    UI update

--- client/src/App.js
+++ client/src/App.js
@@ -5,55 +5,57 @@ import {
     Typography,
     Skeleton,
     Stack,
-    TextField
+    TextField,
+    Grid
 } from "@mui/material";
 import Textarea from '@mui/joy/Textarea';
 
-
 import api from './api';
-
 
 const App = () => {
     const [text, setText] = useState('');
     const [summary, setSummary] = useState('');
-    const [loading, updateLoading] = useState();
+    const [loading, updateLoading] = useState(false);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         updateLoading(true);
-        console.log("text",text);
         const currentText = text;
-        const response = await api.post(`/predict?text=${text}`,currentText,  {headers: { 'Content-Type': 'application/json' }});
+        const response = await api.post(`/predict?text=${text}`, currentText, { headers: { 'Content-Type': 'application/json' } });
         setSummary(response.data);
-        console.log({summary});
         updateLoading(false);
     };
 
     return (
         <Container>
-            <Typography variant="h2" className="heading">Text Summarizer</Typography>
-            <Textarea
-                placeholder="Enter text here..."
-                required
-                sx={{ mb: 1 }}
-                onChange={(e) => setText(e.target.value)}
-
-            />
-            <Button onClick={handleSubmit} variant="contained">Summarize</Button>
-            {loading ? (
-                <Stack>
-                    <Skeleton variant="circular" width={40} height={40} />
-                    <Skeleton />
-                    <Skeleton animation="wave" />
-                    <Skeleton animation="wave" variant="rectangular" height={240}/>
-                </Stack>
-            ) : summary ? (
-                <Stack>
-                    <Typography variant="h3" className="summary">Summary</Typography>
-                    <Typography variant="p">{ summary }</Typography>
-                </Stack>
-            ):null}
-
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <Typography variant="h3" align='center' className="text-to-summarize">Text to Summarize</Typography>
+                    <Textarea
+                        placeholder="Enter text here..."
+                        required
+                        sx={{ mb: 1 }}
+                        onChange={(e) => setText(e.target.value)}
+                        maxRows={25}
+                    />
+                    <Button onClick={handleSubmit} variant="contained">Summarize</Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant="h3" className="summary" align='center'>Summary</Typography>
+                    {loading ? (
+                        <Stack>
+                            <Skeleton variant="circular" width={80} height={80} />
+                            <Skeleton />
+                            <Skeleton animation="wave" />
+                            <Skeleton animation="wave" variant="rectangular" width={640} height={480} />
+                        </Stack>
+                    ) : summary ? (
+                        <Stack>
+                            <Typography variant="body1" contentEditable={true} className="summary-text">{summary}</Typography>
+                        </Stack>
+                    ) : null}
+                </Grid>
+            </Grid>
         </Container>
     );
 
